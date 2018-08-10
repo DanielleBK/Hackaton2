6ae0fce88c28b5053bc6d5a845a5e2eff8efabda
DanielleBK <danielle.bks@gmail.com>
2018-08-10 01:18:30 -0300

    Pagina Masp

---
+++ js/map.js
@@ -0,0 +1,28 @@
+//Create personal icon
+var musicIcon = L.icon({
+    iconUrl: './assets/music_pin.png',
+    iconSize:     [30, 40], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+function loadMap(){
+	//Leaflet Map
+	var initialCoordinates = [-23.5494, -46.6366]; // Sao Paulo
+	var initialZoomLevel = 15;
+
+	// create a map in the "map" div, set the view to a given place and zoom
+	var map = L.map('map').setView(initialCoordinates, initialZoomLevel);
+
+	// add an OpenStreetMap tile layer
+	L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  	attribution: '&copy; Contribuidores do <a href="https://osm.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	pontosTuristicos.forEach(ponto => {
+		let coordinates = [ponto.latitude, ponto.longitude];
+		L.marker(coordinates, {icon: musicIcon}).addTo(map).openPopup();
+	});
+}
+
+loadMap();
